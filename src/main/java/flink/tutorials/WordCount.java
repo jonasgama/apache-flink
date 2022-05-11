7e17e796f2f627744cf1823b99ea874f2a51ff57
Jonas Gama <jonasgamaifs@gmail.com>
2022-05-11 16:46:02 -0300

    first word class in apache flink

--- src/main/java/flink/tutorials/WordCount.java
+++ src/main/java/flink/tutorials/WordCount.java
@@ -1,11 +1,9 @@
 package flink.tutorials;
 
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.UnsortedGrouping;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 
@@ -14,14 +12,17 @@ public class WordCount
     public static void main(String[] args)
             throws Exception
     {
+        //the environment is going to run in the existing jvm
+        //also can be configured to run in a remote environment, like hadoop
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-
+        //read arguments from the java cli, .property files and etc.
         ParameterTool params = ParameterTool.fromArgs(args);
-
+        //turning the params available to each node
         env.getConfig().setGlobalJobParameters(params);
 
         DataSet<String> text = env.readTextFile(params.get("input"));
 
+        //filter all names starting by N
         DataSet<String> filtered = text.filter(new FilterFunction<String>()
 
         {
@@ -30,17 +31,47 @@ public class WordCount
                 return value.startsWith("N");
             }
         });
-        DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
 
+
+        DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
+        //output
+
+        /*
+        *
+        *
+        *   Noman 1
+            Noman 1
+
+        *
+        *   Nanaho 1
+            Noel 1
+            Noman 1
+            Nana 1
+            Nicole 1
+
+        * */
+
+        //group by 0 means, the first element of the stream (string).
+        // them sum up by 1 the second element, in this case a integer
         DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(new int[] { 0 }).sum(1);
+        //output
+
+        /*
+           Noman 12
+        *  Nicole 7
+        *
+        *
+        * */
+
         if (params.has("output"))
         {
+            //to write as csv have to be a tuple
             counts.writeAsCsv(params.get("output"), "\n", " ");
 
             env.execute("WordCount Example");
         }
     }
-
+    //MapFunction generates mulitples outputs, flatmap generates one
     public static final class Tokenizer
             implements MapFunction<String, Tuple2<String, Integer>>
     {
